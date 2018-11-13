b406677b6f1ef84b44af75a7406d9248a7b5d48e
lmvf <lmvf@lmvf.com>
2018-11-13 10:19:23 -0500

    Update 10:19 a.m

---
+++ src/java/Servlets/controlador.java
@@ -0,0 +1,71 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class controlador extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            int opc = Integer.parseInt(request.getParameter("opc"));
+            switch (opc) {
+                case 0:
+                    response.sendRedirect("index.html");
+                    break;
+                default:
+                    out.print("        <h1>[Default] Controlador</h1>\n"
+                            + "        <h2>../controlador?opc=" + opc + "</h2>"
+                            + "        <p>Error en controlador, no se ha encontrado la opción ingresada :(</p>\n"
+                            + "        <a href=\"controlador?opc=0\">Ir al Inicio</a>\n"
+                            + "");
+                    break;
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
